9689de510a1f72373d50e7d294df2dfd882d422e
Robherto <Robhertomatt@gmail.com>
2024-01-26 08:54:05 +0700

    Updte

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
-let username = localStorage.getItem('username');
-document.addEventListener('DOMContentLoaded', () => {
+let username = localStorage.getItem("username");
+document.addEventListener("DOMContentLoaded", () => {
   if (!username) {
-    window.location.href = 'login.html';
+    window.location.href = "login.html";
   }
-  document.getElementById('welcome').innerText = `Welcome, ${username}!`;
+  document.getElementById("welcome").innerText = `Welcome, ${username}!`;
   renderData();
-})
+});
 
-let formBMI = document.getElementById('bmi-form');
-formBMI.addEventListener('submit', (e) => {
+let formBMI = document.getElementById("bmi-form");
+formBMI.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let weights = document.getElementById('berat-badan').value;
-  let heights = document.getElementById('tinggi-badan').value;
-  let bmi = (weights / (heights / 100 * heights / 100)).toFixed(2);
-  let data = localStorage.getItem('data');
+  let weights = document.getElementById("berat-badan").value;
+  let heights = document.getElementById("tinggi-badan").value;
+  let bmi = (weights / (((heights / 100) * heights) / 100)).toFixed(2);
+  let data = localStorage.getItem("data");
 
-  let inputDate = document.getElementById('date').value;
+  let inputDate = document.getElementById("date").value;
   let date = new Date(inputDate).getTime();
-  let tanggal = new Date(date)
-  console.log(tanggal);
+  let tanggal = new Date(date);
+  // console.log(tanggal);
   let id = tanggal.getDate().toString() + tanggal.getMonth().toString() + tanggal.getFullYear().toString();
 
+  /** Ini adalah mebuat data JSON */
 
   if (data !== null && findDataToday(id, JSON.parse(data))) {
-    document.getElementById("notif").innerHTML =
-      `<small>Data at ${tanggal.getDate()}/${tanggal.getMonth() + 1}/${tanggal.getFullYear()} already exist!</small>`;
-  } else if (
-    weights === "" || heights === "" ||
-    weights <= 30 || heights <= 75 ||
-    weights >= 400 || heights >= 250) {
-    document.getElementById("notif").innerHTML =
-      "<small>BB 30-400 kg dan TB 75-250 cm</small>";
+    document.getElementById("notif").innerHTML = `<small>Data at ${tanggal.getDate()}/${tanggal.getMonth() + 1}/${tanggal.getFullYear()} already exist!</small>`;
+  } else if (weights === "" || heights === "" || weights <= 30 || heights <= 75 || weights >= 400 || heights >= 250) {
+    document.getElementById("notif").innerHTML = "<small>BB 30-400 kg dan TB 75-250 cm</small>";
   } else {
     if (data === null) {
-      localStorage.setItem('data', JSON.stringify([{
-        id,
-        date,
-        weights,
-        heights,
-        bmi,
-        status: 'Obese'
-      }]))
+      localStorage.setItem(
+        "data",
+        JSON.stringify([
+          {
+            id,
+            date,
+            weights,
+            heights,
+            bmi,
+            status: "Obese",
+          },
+        ])
+      );
     } else {
       let dataArr = JSON.parse(data);
       dataArr.push({
@@ -50,37 +51,54 @@ formBMI.addEventListener('submit', (e) => {
         weights,
         heights,
         bmi,
-        status: 'Obese'
-      })
-      localStorage.setItem('data', JSON.stringify(dataArr))
+        status: "Obese",
+      });
+      localStorage.setItem("data", JSON.stringify(dataArr));
     }
     renderData();
     document.getElementById("notif").innerHTML = "";
   }
-  document.getElementById('berat-badan').value = "";
-  document.getElementById('tinggi-badan').value = "";
-  document.getElementById('date').value = "";
-})
+  document.getElementById("berat-badan").value = "";
+  document.getElementById("tinggi-badan").value = "";
+  document.getElementById("date").value = "";
+});
 
-let logout = document.getElementById('logout-btn');
-logout.addEventListener('click', () => {
-  localStorage.removeItem('username');
-  localStorage.removeItem('data');
-  window.location.href = 'login.html';
-})
+/** Slesai Pembuatan Data JSON */
+
+let logout = document.getElementById("logout-btn");
+logout.addEventListener("click", () => {
+  localStorage.removeItem("username");
+  localStorage.removeItem("data");
+  window.location.href = "login.html";
+});
+
+function updateModal(id) {
+  let data = localStorage.getItem("data");
+  let arrData = JSON.parse(data);
+  // console.log(arrData);
+  // console.log(arrData[0].id);
+
+  for (let i = 0; i < arrData.length; i++) {
+    if (arrData[i].id === id) {
+      document.getElementById(`berat-aja`).value = arrData[i].weights;
+    }
+  }
+}
+
+function closeModal() {}
 
 function renderData() {
-  let data = localStorage.getItem('data');
-  let dataNotFound = document.getElementById('data-not-found');
-  let bmiTable = document.getElementById('bmi-table');
+  let data = localStorage.getItem("data");
+  let dataNotFound = document.getElementById("data-not-found");
+  let bmiTable = document.getElementById("bmi-table");
   if (data === null || JSON.parse(data).length === 0) {
-    dataNotFound.classList.remove('d-none');
-    bmiTable.classList.add('d-none');
+    dataNotFound.classList.remove("d-none");
+    bmiTable.classList.add("d-none");
   } else {
-    dataNotFound.classList.add('d-none');
-    bmiTable.classList.remove('d-none');
+    dataNotFound.classList.add("d-none");
+    bmiTable.classList.remove("d-none");
     let dataArr = JSON.parse(data);
-    let tableBody = document.getElementById('bmi-table-body');
+    let tableBody = document.getElementById("bmi-table-body");
     tableBody.innerHTML = "";
     for (let i = 0; i < dataArr.length; i++) {
       let tanggal = new Date(dataArr[i].date);
@@ -92,19 +110,18 @@ function renderData() {
           <td>${dataArr[i].bmi}</td>
           <td>${dataArr[i].status}</td>
           <td>
-            <button class="btn btn-outline-info btn-sm"><i class="bi bi-repeat"></i></button>
+            <button onclick="updateModal('${dataArr[i].id}')"  type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Update</button>
             <button class="btn btn-outline-danger btn-sm" onclick="deleteData('${dataArr[i].id}')">
               <i class="bi bi-trash"></i>
             </button>            
           </td>
         </tr>
       `;
-
     }
   }
 }
 function deleteData(id) {
-  let data = localStorage.getItem('data');
+  let data = localStorage.getItem("data");
   let dataArr = JSON.parse(data);
   let result = [];
   for (let i = 0; i < dataArr.length; i++) {
@@ -112,7 +129,7 @@ function deleteData(id) {
       result.push(dataArr[i]);
     }
   }
-  localStorage.setItem('data', JSON.stringify(result));
+  localStorage.setItem("data", JSON.stringify(result));
   renderData();
 }
 function findDataToday(id, data) {
@@ -127,7 +144,7 @@ function findDataToday(id, data) {
 }
 
 function sortByDate() {
-  let data = localStorage.getItem('data');
+  let data = localStorage.getItem("data");
   let dataArr = JSON.parse(data);
   for (let i = 0; i < dataArr.length; i++) {
     for (let j = i + 1; j < dataArr.length; j++) {
@@ -138,6 +155,6 @@ function sortByDate() {
       }
     }
   }
-  localStorage.setItem('data', JSON.stringify(dataArr));
+  localStorage.setItem("data", JSON.stringify(dataArr));
   renderData();
 }
